a38b3a3174b20d29c632ab2bf6aa4da175a87396
Hiroki <1127785+azulamb@users.noreply.github.com>
2017-03-17 17:03:45 +0900

    Add release build command.

--- src/render/message.ts
+++ src/render/message.ts
@@ -2,17 +2,26 @@ const ipcRenderer = require( 'electron' ).ipcRenderer;
 
 class Message
 {
-	private events: { [ keys: string ]: ( event: Electron.IpcRendererEvent, arg: any ) => void };
+	private events:
+	{
+		null: ( event: Electron.IpcRendererEvent, arg: any ) => void,
+		[ keys: string ]: ( event: Electron.IpcRendererEvent, arg: any ) => void,
+	};
 
 	constructor()
 	{
-		this.events = {};
+		this.events = { null: () => {} };
 
 		ipcRenderer.on( 'asynchronous-reply', ( event, arg: { type: string, data: any } ) =>
 		{
-			if ( !this.events[ arg.type ] ) { return; }
+			if ( !this.events[ arg.type ] ) { return this.events.null( event, arg ); }
 			this.events[ arg.type ]( event, arg.data );
 		} );
+	}
+
+	public setDefault( type: string, func: ( event: Electron.IpcRendererEvent, arg: any ) => void )
+	{
+		this.events.null = func;
 	}
 
 	public set( type: string, func: ( event: Electron.IpcRendererEvent, arg: any ) => void )
